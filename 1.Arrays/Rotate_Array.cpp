6ec694d2af507f550fb397fac060970054881915
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2022-05-21 17:40:24 +0530

    Add files via upload

---
+++ 1.Arrays/Rotate_Array.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+/* 
+perforn array rotation 
+eg 2 6 3 1 5 9 8 - input
+d =2  
+1st rotation - 6 3 1 5 9 8 2
+2nd rotation - 3 1 5 9 8 2 6 - answer
+
+*/
+void rotate(int array[], int size,int rotations)
+    {
+       for(int i = 0; i < rotations; i++)
+            { 
+                int start = array[0];
+                for(int j = 0;j<size-1;j++)
+                    {
+                        array[j] = array[j+1];
+                    }
+                array[size-1] = start;
+            }
+    }
+int main()
+    {
+        int array[7];
+        for (int i = 0; i < 7; i++)
+            {
+                cin>>array[i];
+            }
+        cout<<"enter d = ";
+        int d;
+        cin>>d;
+        rotate(array,7,d);
+        for (int i = 0; i < 7; i++)
+            {
+                cout<<array[i]<<" ";
+            }
+        return 0;
+    }
