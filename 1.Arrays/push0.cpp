9ea5150f18b826d043c74f934e89fa6296610bf7
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2022-07-12 20:52:41 +0530

    Update push0.cpp

--- 1.Arrays/push0.cpp
+++ 1.Arrays/push0.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
 using namespace std;
 
-/* push all 0 to the end 
+/* 
+
+push all 0 to the end 
 eg input array = 2 6 0 0 1 9 0 8 0
 output = 2 6 1 9 8 0 0 0 0 
+
 */
 
 int main()
