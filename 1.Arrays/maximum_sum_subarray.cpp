7fb6c9385fdac735fc374fd0c156f5e0dc6a299e
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2022-07-26 10:41:45 +0530

    Add files via upload

---
+++ 1.Arrays/maximum_sum_subarray.cpp
@@ -0,0 +1,35 @@
+//https://leetcode.com/problems/maximum-subarray/
+//solved using kadne's algorithm
+//keep tracking current sum if it becomes less than one discard it and start all over again
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int maxSubArray(vector<int> nums)
+    {
+        int current_sum = 0;
+        int max_sum = INT_MIN;
+        for(int i = 0 ; i<nums.size(); i++)
+            {
+                current_sum += nums[i];
+
+                if(current_sum > max_sum)
+                    {
+                        max_sum = current_sum;
+                    }
+                if(current_sum<0)
+                    {
+                        current_sum = 0;
+                    }
+            }
+        return max_sum;
+    }
+
+int main()
+    {
+        vector<int> nums{5,4,-1,7,8};
+        
+        cout<<maxSubArray(nums);
+    }
+    
