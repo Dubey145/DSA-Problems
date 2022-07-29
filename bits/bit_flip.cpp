892be251df3efedc814e4472e5cc10605fe4c17b
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2022-07-29 17:53:08 +0530

    Bit magic

---
+++ bits/bit_flip.cpp
@@ -0,0 +1,26 @@
+//https://leetcode.com/problems/minimum-bit-flips-to-convert-number/
+
+#include<iostream>
+using namespace std;
+
+int minBitFlips(int start, int goal) 
+    {
+        int count = 0 ;
+        int number = start^goal;
+        while(number != 0)
+            {
+                if(number&1 == 1)
+                    {
+                        count++;
+                    }
+                number = number>>1;
+            }
+        
+        return count;
+    }
+int main()
+    {
+        int start = 5;
+        int end = 7;
+        cout<<minBitFlips(start,end);
+    }
