a4b860121eb39ea15755cd05099b90256493b2b3
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2022-08-01 12:50:46 +0530

    max consecutive ones

---
+++ 1.Arrays/max_consec_ones.cpp
@@ -0,0 +1,33 @@
+// problem link : 
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int findMaxConsecutiveOnes(vector<int>& nums) 
+    {
+        int result =  0;
+        int current_max = 0;
+        
+        for(int i = 0 ; i<nums.size();i++)
+            {
+                if(nums[i] == 1)
+                    {
+                        current_max++;
+                        result = max(current_max,result);
+                    }
+                else
+                    {
+                        current_max = 0;
+                    }
+
+            }
+        return result;
+    }
+int main()
+    {
+        vector<int> nums{1,1,0,1,1,1};
+        cout<<findMaxConsecutiveOnes(nums);
+
+        return 0;
+    }
