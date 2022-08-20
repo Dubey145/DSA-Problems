52a1982ec12fe20048c01d6c2d23a4e2529677bf
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2022-08-20 21:45:42 +0530

    Add files via upload

---
+++ 1.Arrays/Lemonade_Change.cpp
@@ -0,0 +1,52 @@
+// problem link : https://leetcode.com/problems/lemonade-change/
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+bool lemonadeChange(vector<int>& bills) 
+    {
+        int fives = 0;
+        int tens = 0;
+        int twenties = 0;
+        for(int i = 0 ; i<bills.size();i++)
+            {
+                if(bills[i] == 5) fives++;
+
+                else if(bills[i] == 10)
+                    {
+                        if(fives<1)
+                            return false;
+                        fives--;
+                        tens++;
+                    }
+                else
+                    {
+                        //20
+                        //either give one tens and one fives 
+                        //or 3 fives
+                        twenties++;
+                        if(tens>=1 && fives>=1)
+                            {
+                                tens--;
+                                fives--;
+                            }
+                        else if(fives>=3)
+                            {
+                                fives -=3;
+                            }
+                        else 
+                            {
+                                return false;
+                            }
+                    }
+            }
+        return true;
+    }
+int main()
+    {
+        vector<int> nums{5,5,5,10,20};
+        cout<<lemonadeChange(nums);
+
+        return 0;
+    }
