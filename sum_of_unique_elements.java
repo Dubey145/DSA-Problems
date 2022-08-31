3a78855114734f56c4e3714b754070fe281b73ce
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2022-08-31 17:55:40 +0530

    Add files via upload

---
+++ sum_of_unique_elements.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class sum_of_unique_elements{
+    
+    public int sumOfUnique(int[] nums) {
+        Set<int> sum = new HashSet<int>();
+        for(int i = 0; i < nums.length();i++){
+            set.add(i);
+        }
+        int result = 0;
+        for(int number : sum){
+            sum+=number;
+        }
+        return number;
+    }
+
+    public static void main(String[] args) throws Exception{
+        
+    }
+}
