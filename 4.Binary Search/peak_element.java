1faf700030e0f9cf79415d4e28032c57b122dad7
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2022-09-03 20:47:05 +0530

    Add files via upload

--- 4.Binary Search/peak_element.java
+++ 4.Binary Search/peak_element.java
@@ -38,11 +38,8 @@ public class peak_element{
     public static int get_peak(int[] nums){
         int start = 0;
         int end = nums.length-1;
-
         while(start<end){
-
             int mid = start + (end-start)/2;
-            
             if(nums[mid]<nums[mid+1]){
                 start = mid+1;
             }
