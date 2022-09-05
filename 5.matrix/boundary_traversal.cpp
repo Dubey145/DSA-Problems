064141dc0dfe2cc4f269155c4926d8b061d60a8b
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2022-09-05 16:56:59 +0530

    Add files via upload

--- 5.matrix/boundary_traversal.cpp
+++ 5.matrix/boundary_traversal.cpp
@@ -6,6 +6,26 @@ using namespace std;
 
 void boundaryTraversal(vector<vector<int>> nums)
     {
+        //handle edge case of single row or single column
+        //matrices are assumed to have equal number of elements in rows 
+
+        if(nums.size() == 1) // only one row
+            {
+                for(int i = 0 ; i < nums[i].size(); i++)
+                    {
+                        cout<<nums[0][i];
+                    }
+                return;
+            }
+        if(nums[0].size() == 1) // only one column 
+            {
+                for(int i = 0 ; i < nums.size(); i++)
+                    {
+                        cout<<nums[i][0];
+                    }
+                return;
+            }
+
         for(int i = 0 ; i < nums.size();i++)
             {
                 cout<<nums[0][i]<<" ";
@@ -31,6 +51,7 @@ void boundaryTraversal(vector<vector<int>> nums)
 int main()
     {
         vector<vector<int>> nums{{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+        nums = {{1},{2},{3},{4}};
         boundaryTraversal(nums);
 
 
