07170f7e24f5167be217c477967bf75a051a447f
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2022-09-22 17:48:57 +0530

    Add files via upload

---
+++ 0_sum_subarr.cpp
@@ -0,0 +1,31 @@
+// problem link : 
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<unordered_set>
+using namespace std;
+
+bool zero_sub(vector<int> &nums)
+    {
+        unordered_set<int> set;
+        int prefix_sum = 0;
+
+        for(int i = 0 ; i < nums.size(); i++)
+            {
+                prefix_sum += nums[i];
+                if(prefix_sum == 0)
+                    return true;
+                if(set.find(prefix_sum) != set.end())
+                    return true;
+                set.insert(prefix_sum);
+            }
+        return false;
+    }
+
+int main()
+    {
+        vector<int> nums{3,4,3,1,0};
+        cout<<zero_sub(nums);
+
+        return 0;
+    }
