f9096b5f01731ab3126a4e32a1ba974dceab4638
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2022-09-27 05:42:17 +0530

    destination city

---
+++ hashing/destination_city.cpp
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/destination-city/submissions/
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) 
+    {
+        unordered_set<string> start;
+        unordered_set<string> end;
+        
+        for(int i = 0; i < paths.size(); i++)
+        {
+            start.insert(paths[i][0]);
+            end.insert(paths[i][1]);
+        }
+        
+        for(auto it : end)
+        {
+            if(start.find(it) == start.end())
+            {
+                return it;
+            }
+        }
+        return "";
+    }
+};
