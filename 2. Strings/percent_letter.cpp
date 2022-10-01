86bacfb475f75ff40a74df881c6357aead9adffe
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2022-10-01 22:30:23 +0530

    Create percent_letter.cpp

---
+++ 2. Strings/percent_letter.cpp
@@ -0,0 +1,14 @@
+//%of letter ez
+class Solution {
+public:
+    int percentageLetter(string s, char letter) {
+        int count = 0;
+        for(char c : s)
+        {
+            if(c == letter)
+                count++;
+        }
+        int percent = (count*100)/s.size();
+        return percent;
+    }
+};
