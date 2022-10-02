d50410638131948eb6b92b450b8809075c141ea2
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2022-10-02 13:04:24 +0530

    Add files via upload

---
+++ 2. Strings/add_2_strings.cpp
@@ -0,0 +1,39 @@
+// problem link : https://leetcode.com/problems/add-strings/submissions/
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+string addString(string num1, string num2)
+    {
+        string result;
+        int i = num1.size()-1;
+        int j = num2.size()-1;
+        int carry = 0;
+
+        while(i>= 0 || j>= 0)
+            {
+                int sum = carry;
+                if(i>=0)
+                    {
+                        sum+= num1[i--] - '0';
+                    }
+                if(j>=0)
+                    {
+                        sum+= num2[j--] - '0';
+                    }
+                carry = sum>9?1:0;
+                result += to_string(sum%10);
+            }    
+        if(carry) result += to_string(carry);
+        reverse(result.begin(),result.end());
+        return result;
+    }
+
+int main()
+    {
+        vector<int> nums{};
+
+
+        return 0;
+    }
