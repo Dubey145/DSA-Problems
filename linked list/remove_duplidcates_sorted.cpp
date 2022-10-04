57f1db581f2d420490850f0e3d586d5a6eb6a56c
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2022-10-04 23:45:36 +0530

    Create remove_duplidcates_sorted.cpp

---
+++ linked list/remove_duplidcates_sorted.cpp
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* temp = head;
+        if(head == NULL || head->next == NULL)
+            return head;
+        while(temp->next!=NULL)
+        {
+            if(temp->val == temp->next->val)
+                    temp->next = temp->next->next;
+            else
+                temp = temp->next;
+        }
+        return head;
+    }
+};
