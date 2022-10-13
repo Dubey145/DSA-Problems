ed65c5ff25d9e781839dd5e45c1140caec4062e3
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2022-10-13 07:34:51 +0530

    Add files via upload

--- stack/stl_stack.cpp
+++ stack/stl_stack.cpp
@@ -2,13 +2,22 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<stack>
 using namespace std;
 
 //copy function signature
 
 int main()
     {
-        vector<int> nums{};
+        stack<int> s;
+        s.push(10);
+        s.push(20);
+        s.push(30);
+
+        cout<<s.size()<<endl;
+        cout<<s.top()<<endl;
+        s.pop();
+        cout<<s.top()<<endl;
 
 
         return 0;
