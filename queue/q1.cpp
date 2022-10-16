0d7fd7d18ee544ff95a8edb3e5f5d5207f334c77
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2022-10-16 22:04:44 +0530

    Add files via upload

---
+++ queue/q1.cpp
@@ -0,0 +1,35 @@
+// problem link : Queue
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+/*
+A queue is a first in first out data structure
+dequeue  <- 1,2,3,4 <- enqueue 
+it is like a pipe
+
+deque() deletes from the front
+enqueue() add an element at the rear
+getFront() - gets the front item
+getRear() - gets last element
+size() - gives size
+isempty() - returns true if q is empty
+
+
+Applications : 
+1. single resource - multiple consumers
+2. synchronization between slow and fast devices
+3. Scheduling algorithms 
+4. Computer networks - switches / router/ mail queue
+5. Variations - Deque, Priority Q and doubly ended priority q
+in deque - insertion and deletion at both the end
+Priority q - implemented using heaps (doctor-patients analogy) serves more important patient first
+*/
+
+int main()
+    {
+
+
+        return 0;
+    }
