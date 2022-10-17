5752d0d81943486af44b49c8dd7805c7aa74b019
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2022-10-17 06:25:40 +0530

    Add files via upload

---
+++ queue/stl_q.cpp
@@ -0,0 +1,28 @@
+// problem link : 
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+using namespace std;
+
+/*
+    queue<int> q
+normal queue of first in first out
+push() - adds an element at the rear - enqueue
+pop - deletes from front - dequeue
+front - gives the first item 
+back - gives the last item 
+empty - returns a boolean value
+size - gives the size of the q
+
+ALL operations have TC O(1)
+it is implemented using list or dequeue(default)
+*/
+
+int main()
+    {
+        vector<int> nums{};
+
+
+        return 0;
+    }
