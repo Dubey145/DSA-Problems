73d134b8454f0e41c27385526c5841ebbc320318
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2022-10-21 19:38:22 +0530

    Create stone_game.cpp

---
+++ 1.Arrays/stone_game.cpp
@@ -0,0 +1,89 @@
+//https://leetcode.com/problems/stone-game/
+
+class Solution {
+public:
+    bool stoneGame(vector<int>& nums) 
+    {
+//         int alice_sum = 0;
+//         int bob_sum = 0;
+        
+//         int i = 0;
+//         int j = nums.size()-1;
+        
+//         bool flag = true;
+        
+//         while(i<j)
+//         {
+//             int current = 0;
+//             if(nums[i]>=nums[j])
+//             {
+//                 current=nums[i];
+//                 i++;
+//             }
+//             else 
+//             {
+//                 current =  nums[j];
+//                 j--;
+//             }
+//             if(flag)
+//             {
+//                 alice_sum+=current;
+//                 flag = false;
+//             }
+//             else 
+//             {
+//                 bob_sum+=current;
+//                 flag = true;
+//             }
+//             //3+3
+//             //7+2
+            
+//             //3+7 
+//             //3+5 
+//         }
+        
+//         cout<<alice_sum<<" "<<bob_sum<<endl;
+        
+//         i=0;
+//         j=nums.size()-1;
+//         int alice2=0;
+//         int bob2 =0;
+//         flag = true;
+//         while(i<j)
+//         {
+//             int current = 0;
+//             if(nums[i]>nums[j])
+//             {
+//                 current=nums[i];
+//                 i++;
+//             }
+//             else 
+//             {
+//                 current =  nums[j];
+//                 j--;
+//             }
+//             //cout<<current<<endl;
+//             if(flag)
+//             {
+//                 alice2+=current;
+//                 flag = false;
+//             }
+//             else 
+//             {
+//                 bob2+=current;
+//                 flag = true;
+//             }
+//             //3+3
+//             //7+2
+            
+//             //3+7 
+//             //3+5 
+//         }
+//         cout<<alice2<<" "<<bob2;
+        
+//         return max(alice_sum,alice2)>max(bob_sum,bob2);
+        
+        //what the fu
+        return true;
+    }
+};
