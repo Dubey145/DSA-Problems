a195617246671b8b38e70e3237292e4cb87d8fe4
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2022-10-26 22:43:10 +0530

    Update greatest_candies.java

--- 1.Arrays/greatest_candies.java
+++ 1.Arrays/greatest_candies.java
@@ -1,6 +1,7 @@
 import java.util.*;
 class greatest_candies{
     public static List<Boolean> kidsWithCandies(int[] candies,int extraCandies){
+        
         List<Boolean> result = new ArrayList<Boolean>();
         int max_val=candies[0];
         for(int i = 1; i < candies.length; i++ ){
