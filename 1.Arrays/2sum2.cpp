04c0164e46d073f05a7ac0027b45273cce5ae9c1
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2022-11-18 19:38:56 +0530

    Create 2sum2.cpp

---
+++ 1.Arrays/2sum2.cpp
@@ -0,0 +1,30 @@
+
+//https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> indices;
+        int start = 0;
+        int end = nums.size()-1;
+        
+        while(start<=end)
+        {
+            if(nums[start]+nums[end] < target)
+            {
+                start++;
+            }
+            else if(nums[start]+nums[end] > target)
+            {
+                end--;
+            }
+            else
+            {
+                indices.push_back(start+1);
+                indices.push_back(end+1);
+                break;
+            }
+        }
+        return indices;
+    }
+};
