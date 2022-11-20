8feb15145d12cb2b4dafdaf1f93b3ce5b69d7718
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2022-11-20 22:33:36 +0530

    Create kth_smallest.cpp

---
+++ trees/kth_smallest.cpp
@@ -0,0 +1,32 @@
+//https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    vector<int> elements;
+    
+    void setMin(TreeNode * root)
+    {
+        if(root == NULL) return;
+        
+        setMin(root->left);
+        elements.push_back(root->val);
+        setMin(root->right);
+    }
+    int kthSmallest(TreeNode* root, int k) 
+    {
+        setMin(root);
+        return elements[k-1];    
+    }
+};
