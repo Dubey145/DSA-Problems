9243a014e03f6fa81760573bdf4ddcc0219f34db
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2022-11-28 20:11:32 +0530

    Create hamming.cpp

---
+++ 6. Bit Magic/hamming.cpp
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/number-of-1-bits/
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) 
+    {
+        int count = 0;
+        for(int i = 0; i<32;i++)
+        {
+            if(n&1) count++;
+            
+            n=n>>1;
+        }
+        return count;
+    }
+};
