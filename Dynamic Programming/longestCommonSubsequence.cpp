7d562cc883a20e31b200bf36595edf7fd47d2954
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2022-12-07 20:32:57 +0530

    Create longestCommonSubsequence.cpp

---
+++ Dynamic Programming/longestCommonSubsequence.cpp
@@ -0,0 +1,28 @@
+//https://leetcode.com/problems/longest-common-subsequence
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) 
+    {
+        int m = word1.size();
+        int n = word2.size();
+        int dp[m+1][n+1];
+        for(int i = 0; i <= m;i++)dp[i][0] = 0;
+        for(int i = 0; i <= n;i++)dp[0][i] = 0;
+
+        for(int i = 1; i<=m ; i++)
+            {
+                for(int j = 1; j <= n; j++)
+                    {
+                        if(word1[i-1] == word2[j-1])
+                            dp[i][j] = 1 + dp[i-1][j-1];
+                        else
+                            {
+                                dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                            }
+                    }
+            }
+        return (m-dp[m][n]) + (n-dp[m][n]);
+
+    }
+};
