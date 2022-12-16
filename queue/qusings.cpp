dc0966b7b68b8f24a4823d01d2fdbe7a46a801e3
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2022-12-16 22:09:53 +0530

    Create qusings.cpp

---
+++ queue/qusings.cpp
@@ -0,0 +1,54 @@
+class MyQueue {
+public:
+    //take the bottom of the stack as the rear 
+    stack<int>s1;
+    stack<int>s2;
+
+    MyQueue() 
+    {
+
+    }
+    
+    void push(int x) 
+        {
+            while(!s1.empty())
+                {
+                    int current = s1.top();
+                    s1.pop();
+                    s2.push(current);
+                }
+            s1.push(x);
+            while(!s2.empty())
+                {
+                    int current = s2.top();
+                    s2.pop();
+                    s1.push(current);
+                }
+        }
+    
+    int pop() 
+        {
+            int deleted = s1.top();
+            s1.pop();
+            return deleted;
+        }
+    
+    int peek() 
+    {
+        return s1.top();    
+    }
+    
+    bool empty() 
+    {
+        return s1.empty();    
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
