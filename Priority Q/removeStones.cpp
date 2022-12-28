211d6ffe966e340788650a108b8e1bd8f237016f
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2022-12-28 18:01:58 +0530

    Create removeStones.cpp

---
+++ Priority Q/removeStones.cpp
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/remove-stones-to-minimize-the-total/
+
+class Solution {
+public:
+    int minStoneSum(vector<int>& piles, int k) 
+    {
+        int sum = 0;
+
+        for(int pile : piles) sum += pile;
+
+        priority_queue<int> pq(piles.begin(),piles.end());
+
+        while(!pq.empty() && k--)
+            {
+                int current = pq.top();
+                pq.pop();
+                
+                sum -= current/2;
+                current -= current/2;
+
+                pq.push(current);
+            }
+        return sum;
+    }
+};
